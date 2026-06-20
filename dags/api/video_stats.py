1700115f9a3d3e6204aae025d49a7c97d231d6f3
hulisani <rachyhuly17@gmail.com>
2026-06-20 22:11:04 +0200

    feat: add helper functions to populate data to postegres

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -1,16 +1,20 @@
 import requests
 import json
-import os
+
 from datetime import date
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
+# import os
+# load_dotenv(dotenv_path="./.env")
 
-load_dotenv(dotenv_path="./.env")
+from airflow.decorators import task
+from airflow.models import Variable
 
-API_KEY = os.getenv("API_KEY")
+API_KEY = Variable.get("API_KEY")
 
-CHANNEL_HANDLE = "Latestsightings"
+CHANNEL_HANDLE = Variable.get("CHANNEL_HANDLE")
 maxResults = 50
 
+@task
 def get_playlist_id():
     try:
 
@@ -33,7 +37,7 @@ def get_playlist_id():
         raise e
 
 
-
+@task
 def get_video_ids(playlistId):
     video_ids =[]
     pageToken = None
@@ -64,7 +68,7 @@ def get_video_ids(playlistId):
     except requests.exceptions.RequestException as e:
         raise e
 
-
+@task
 def extract_video_data(video_ids):
     extracted_data = []
 
@@ -101,7 +105,7 @@ def extract_video_data(video_ids):
 
     except requests.exceptions.RequestException as e:
         raise e 
-    
+@task    
 def save_to_json(extracted_data):
     file_path = f"./data/YT_data_{date.today()}.json"
     with open(file_path, "w", encoding="utf-8") as json_outfiles:
